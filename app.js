957805b378f2543f54da73aea2228050119d9b00
surajkr20 <suraj71308kumar@gmail.com>
2025-07-31 05:25:05 +0530

    creating server and routing with http

--- app.js
+++ app.js
@@ -2,9 +2,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) =>{
-    res.end("Good Morning..")
+    if(req.url == "/about"){
+        console.log("hello i'm abhinav bhardwaj")
+    }
+    if(req.url == "/contact"){
+        console.log("i'm alian, i have not contact details")
+    }
+    res.end("request heated on the server");
 })
 
-server.listen(3000, ()=>{
-    console.log("server started at port 3000")
+server.listen(3000, (req, res) =>{
+    console.log("server staretd")
 })
+
