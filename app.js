ba4fd6c24123dfec918a8506d6a18fc041864aef
surajkr20 <suraj71308kumar@gmail.com>
2025-07-31 06:59:38 +0530

    middlewares and readme file(all learning notes)

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 
 const express = require("express");
 const app = express();
+const morgon = require('morgan');
 
-app.get("/about", (req,res) =>{
-    res.send("this is about page")
+app.set("view engine", 'ejs')   // setup ejs files
+
+// A middleware is a function that runs in between the request from the user and the final response from the server.
+
+// This is a global middleware(app.use) — it runs for every route.
+app.use((req, res, next) =>{
+    // res.send("this is a middleware") // after execute this line no request goes next route
+    const a = 2;
+    const b = 4;
+    console.log(a + b)
+    return next();
 })
 
-app.get('/profile', (req, res) =>{
-    res.send("getting profile")
+app.get('/', (req, res) =>{
+    res.render('index')
 })
 
 app.listen(3000, (req, res) =>{
