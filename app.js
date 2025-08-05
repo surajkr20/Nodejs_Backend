415327895bb296cfdafddb8399c8677872a85709
surajkr20 <suraj71308kumar@gmail.com>
2025-08-05 06:36:50 +0530

    third party middleware - morgan

--- app.js
+++ app.js
@@ -3,21 +3,24 @@ const express = require("express");
 const app = express();
 const morgon = require('morgan');
 
+app.use(morgon('dev')) // third party middleware - logger
+
 app.set("view engine", 'ejs')   // setup ejs files
 
 // A middleware is a function that runs in between the request from the user and the final response from the server.
 
 // This is a global middleware(app.use) — it runs for every route.
 app.use((req, res, next) =>{
-    // res.send("this is a middleware") // after execute this line no request goes next route
-    const a = 2;
-    const b = 4;
-    console.log(a + b)
     return next();
+    res.send("this is a middleware") // after execute this line no request goes next route
 })
 
 app.get('/', (req, res) =>{
     res.render('index')
+})
+
+app.get('/about', (req, res) =>{
+    res.render('about')
 })
 
 app.listen(3000, (req, res) =>{
